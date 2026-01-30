7790c5729571f6acb4f1c82e2a9209ecec54d0b7
Safieta <safieta67@gmail.com>
2026-01-30 08:51:13 +0000

    Add root health route

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend LLM Gemini OK 🚀');
+});
+
 app.use('/api/recipes', recipeRoutes);
 
 const PORT = process.env.PORT || 5000;
